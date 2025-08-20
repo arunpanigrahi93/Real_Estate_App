e8b4a49a89cdae267c607092f40fa354f7112e6e
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-20 15:46:17 +0530

    Header and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 export default App;
