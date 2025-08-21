aeeb8898d7b0889b55e0e52722fb8f6ac40c4e2c
arunpanigrahi93 <arunpanigrahi94@gmail.com>
2025-08-21 21:03:36 +0530

    update Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { assets, projectsData } from "../assets/assets";
 
 const Projects = () => {
